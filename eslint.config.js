c9f7e8df09a0ca82a368a5bfc9fc5c529ecdb30d
Felipe Cesar <felipecesr@gmail.com>
2025-02-03 18:20:33 -0300

    fix config prettier import

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import testingLibrary from "eslint-plugin-testing-library";
-const eslintConfigPrettier = require("eslint-config-prettier");
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
   { ignores: ["dist"] },
