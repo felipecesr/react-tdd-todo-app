e4dbfbc18afda6225c505e9c3e247dccb9fe6c3f
Felipe Cesar <felipecesr@gmail.com>
2025-02-08 06:52:17 -0300

    remove eslint testing library plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import testingLibrary from "eslint-plugin-testing-library";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
@@ -26,10 +25,6 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  },
-  {
-    files: ["**/*.test.{ts,tsx}"],
-    ...testingLibrary.configs["flat/react"],
   },
   eslintConfigPrettier,
 );
